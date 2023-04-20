7293aa9d8ce9313a39c659bfe7f013ea97684ffa
Alapan Mukherjee <alapan.mukherjee@futurice.com>
2023-04-20 22:29:23 +0300

    WIP Fetch artists info

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField } from '@mui/material';
 import { Autocomplete } from '@mui/material';
 import { Artist } from './types';
 import './styles/App.scss';
 
 function App() {
+  const [ artists, setArtists ] = useState([]);
+
+  useEffect(() => {
+
+  });
+
   return (
     <div className='container'>
       <Autocomplete
@@ -17,7 +23,5 @@ function App() {
     </div>
   );
 }
-
-const artists: Artist[] = [];
 
 export default App;
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,18 @@
+export interface ArtistImage {
+  url: string;
+  height: number;
+  width: number;
+}
+
+export interface Followers {
+  total: number;
+  href: string;
+}
+
 export interface Artist {
   name: string;
+  id: string;
+  genres: string[];
+  href: string;
+  images: ArtistImage[];
 }
